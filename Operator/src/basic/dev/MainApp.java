9227fe7aa1a125d5597f4222251e6596d8a51c78
nguyenhuuanhkhoa <khoaanguyenn5@gmail.com>
2024-12-22 16:09:54 +0700

    Add files via upload

---
+++ Operator/src/basic/dev/MainApp.java
@@ -0,0 +1,15 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b, c, d;
+		b = c = d = a;
+		String str = a < b ? "a lon hon b" : "b hon lon a";
+		System.out.println(str);
+
+	}
+
+}
